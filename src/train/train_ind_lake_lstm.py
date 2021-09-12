b2643983394a1d7000d471c73ddd9b145065a9a8
Jared W <willa099@umn.edu>
2021-09-12 18:43:23 -0500

    move to remote

--- src/train/train_ind_lake_lstm.py
+++ src/train/train_ind_lake_lstm.py
@@ -103,6 +103,7 @@ trn_dates = np.load(trn_dates_path, allow_pickle=True)
 tst_data_raw = torch.from_numpy(np.array(np.load(tst_data_path,allow_pickle=True),dtype=np.float32))
 tst_data = torch.from_numpy(np.array(np.load(tst_norm_data_path,allow_pickle=True),dtype=np.float32))
 tst_dates = np.load(tst_dates_path, allow_pickle=True)
+pdb.set_trace()
 ###############################
 # data preprocess
 ##################################
