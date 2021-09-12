2b1837439117deb9360693b3aa06ace6d2fd097e
Jared W <willa099@umn.edu>
2021-09-12 12:46:44 -0500

    move to remote

--- src/evaluate/compileResults.py
+++ src/evaluate/compileResults.py
@@ -5,6 +5,11 @@ site_ids = np.load("../../metadata/201site_ids.npy",allow_pickle=True)
 raw_data_dir = '../../data/raw/DOzip/'
 process_rmses = []
 
+def rmse(pred,targ):
+	return np.sqrt(((pred - targ)**2).mean())
 for site_id in site_ids:
 	site_df = pd.read_feather(raw_data_dir+site_id+"/"+site_id+".feather")
+
+	#filter to test obs
+	site_df = site_df[site_df['splitsample']==1]
 	pdb.set_trace()
