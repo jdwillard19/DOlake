480aa3fde3dfd96ee63ff648f7a3d1c342e0d1ff
Jared W <willa099@umn.edu>
2021-09-12 12:49:34 -0500

    move to remote

--- src/evaluate/compileResults.py
+++ src/evaluate/compileResults.py
@@ -7,13 +7,18 @@ process_rmses = []
 
 def rmse(pred,targ):
 	return np.sqrt(((pred - targ)**2).mean())
-for site_id in site_ids:
+for ct, site_id in enumerate(site_ids):
+	print(ct,"/",len(site_ids), " sites: ",site_id)
 	site_df = pd.read_feather(raw_data_dir+site_id+"/"+site_id+".feather")
 
 	#filter to test obs
-	site_df = site_df[(site_df['splitsample']==1) & (pd.notnull(site_df['obs_hyp']))]
+	site_df_test = site_df[(site_df['splitsample']==1) & (pd.notnull(site_df['obs_hyp']))]
 
-	process_rmses.append(rmse(site_df['obs_hyp'], site_df['o2_hyp']))
+	site_rmse = rmse(site_df_test['obs_hyp'], site_df_test['o2_hyp'])
 
+	if np.isnan(site_rmse):
+		print("nan?")
+		pdb.set_trace()
+	process_rmses.append(site_rmse)
 
 pdb.set_trace()
