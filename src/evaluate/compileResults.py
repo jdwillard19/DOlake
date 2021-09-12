4570c21d89d00d849302ca7dfe4aecdb7348fd73
Jared W <willa099@umn.edu>
2021-09-12 12:40:51 -0500

    move to remote

---
+++ src/evaluate/compileResults.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+
+site_ids = np.load("../../metadata/201site_ids.npy",allow_pickle=True)
+raw_data_dir = '../../data/raw/DOzip/'
+process_rmses = []
+
+for site_id in site_ids:
+	site_df = pd.read_feather(raw_data_dir+site_id+"/"+site_id+".feather")
+	pdb.set_trace()
