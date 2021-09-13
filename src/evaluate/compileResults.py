241b12374209152ff0fffe00105c9191d9e32613
Jared W <willa099@umn.edu>
2021-09-13 12:38:36 -0500

    move to remote

--- src/evaluate/compileResults.py
+++ src/evaluate/compileResults.py
@@ -6,10 +6,15 @@ raw_data_dir = '../../data/raw/DOzip/'
 process_rmses = []
 
 sites_without_data = ['nhdhr_121857622','nhdhr_120018361','nhdhr_126212479','nhdhr_120018006']
-site_ids = site_ids[~np.isin(site_ids,sites_without_data)]
+sites_without_data2 = sites_without_data + ['nhdhr_120018027', 'nhdhr_120020360', 'nhdhr_120020376',\
+										    'nhdhr_120018182', 'nhdhr_120018012', 'nhdhr_120017997',\
+										    'nhdhr_120018017', 'nhdhr_105231881', 'nhdhr_120020352',\
+										    'nhdhr_120017997']
+site_ids = site_ids[~np.isin(site_ids,sites_without_data2)]
 def rmse(pred,targ):
 	return np.sqrt(((pred - targ)**2).mean())
 
+df = pd.DataFrame()
 for ct, site_id in enumerate(site_ids):
 	print(ct,"/",len(site_ids), " sites: ",site_id)
 	site_df = pd.read_feather(raw_data_dir+site_id+"/"+site_id+".feather")
@@ -23,5 +28,10 @@ for ct, site_id in enumerate(site_ids):
 		print("nan?")
 		pdb.set_trace()
 	process_rmses.append(site_rmse)
+
+    lstm_df = pd.read_feather("../../results/singleSiteLSTM_"+site_id+".feather")
+
+    pdb.set_trace()
+
 
 pdb.set_trace()
